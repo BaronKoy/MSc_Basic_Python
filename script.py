478e8bd07071c22e447d46f4e9c3fe254d1d2c2d
BaronKoy <baron.koylass.16@ucl.ac.uk>
2016-12-08 18:30:49 +0000

    add script.py

---
+++ script.py
@@ -0,0 +1,18 @@
+from os.path import basename
+f= open("input_file.txt")
+x=[]
+for l in f.readlines():
+	x.append(l.strip())
+
+
+d=dict()
+for a in x:
+	y=basename(a).split('_')
+	sample=y[0]
+	if sample not in d:
+		d[sample]=[a]
+	else:
+		d[sample].append(a)
+
+print d
+	
