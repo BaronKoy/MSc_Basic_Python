b9701e68ea1f7955631446114f3fff846e0b1869
Baron Koylass <baron.koylass.16@ucl.ac.uk>
2017-04-21 16:32:54 +0100

    Phenopolis information scripts
    Scripts in order to sort Phenopolis variant information

---
+++ Phenopolis_patient_scores.py
@@ -0,0 +1,53 @@
+#calling HPO terms
+
+#packages
+import requests
+import pymongo
+import json
+import sys
+
+#database
+client = pymongo.MongoClient(host='phenotips.cs.ucl.ac.uk', port=27017)
+patients_db = client['patients']
+
+
+"""example of JSON decoder
+r = requests.get('https://api.github.com/events')
+r.json()"""
+
+
+
+#NP example for one patient
+for patient in patients_db.patients.find():
+    p=patient
+    x=dict()
+    x['id']=p['report_id']
+    x['features']=p['features']
+    for f in p['features']:
+        f['isPresent']={'yes':'true','no':'false'}[f['observed']]
+        del f['observed']
+        del f['label']
+        del f['type']
+	y=json.dumps(x)
+	url='https://monarchinitiative.org/score/?annotation_profile={}'.format(y)
+    r=requests.get(url,headers={'Content-Type':'application/json'})
+    d=r.json()
+    print p['external_id'],d['simple_score']
+    break
+
+#Listing patients external_id with simple_score
+for patient in patients_db.patients.find():
+    p=patient
+    x=dict()
+    x['id']=p['report_id']
+    x['features']=p['features']
+    for f in p['features']:
+        f['isPresent']={'yes':'true','no':'false'}[f['observed']]
+        del f['observed']
+        del f['label']
+        del f['type']
+	y=json.dumps(x)
+	url='https://monarchinitiative.org/score/?annotation_profile={}'.format(y)
+    r=requests.get(url,headers={'Content-Type':'application/json'})
+    d=r.json()
+    print p['external_id'],d['simple_score']
