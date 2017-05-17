57b8828b55f714222bcdba4747aa76948b898abf
Baron Koylass <baron.koylass.16@ucl.ac.uk>
2017-05-17 17:01:16 +0100

    Extracting ClinVar I.D from gene json files

--- ClinVar_Script.py
+++ ClinVar_Script.py
@@ -9,7 +9,6 @@ with open('TTLL5.json') as json_data:
     tree = objectpath.Tree(d)
     for x in (tuple(tree.execute('$..sub'))):
         if str(x).startswith('ClinVarVariant'): print(x)
-    raise Exception('jk')
 
 for ClinVar in json_data.find():
     if 'ClinVarVariant' in ClinVar:
