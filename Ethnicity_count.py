c89237f4cfdf4670f8b43c31368b0736bd986691
Baron Koylass <baron.koylass.16@ucl.ac.uk>
2017-06-20 17:55:59 +0100

    Update Ethnicity_count.py

--- Ethnicity_count.py
+++ Ethnicity_count.py
@@ -10,12 +10,13 @@ import operator
 #AMR: Latino
 #AFR: African/African American
 #OTH: Other
-#raw: raw count
-#Female: Female
 #EAS: East Asian
 #ASJ: Ashkenazi Jewish
 #NFE: Non-Finnish European
- 
+#raw: raw count
+#Female: Female count
+#Male: Male count 
+
 total=dict()
 figs=dict()
 extra=dict()
@@ -25,11 +26,12 @@ count_AMR=0
 count_AFR=0
 count_OTH=0
 count_raw=0
-count_Female=0
 count_EAS=0
 count_ASJ=0
 count_NFE=0
- 
+count_Female=0
+count_Male=0
+
 for data in sys.stdin:
     data=data.strip()
     data=json.loads(data)
@@ -46,7 +48,7 @@ for data in sys.stdin:
          figs={'AF_FIN': nums['AF_FIN'],'AF_AMR': nums['AF_AMR'],'AF_AFR': nums['AF_AFR'],
          'AF_OTH': nums['AF_OTH'], 'AF_EAS': nums['AF_EAS'],'AF_ASJ': nums['AF_ASJ'],
          'AF_NFE':nums['AF_NFE']}
-         extra={'AF_raw': nums['AF_raw'],'AF_Female': nums['AF_Female']}
+         extra={'AF_raw': nums['AF_raw'],'AF_Female': nums['AF_Female'],'AF_Male': nums['AF_Male']}
          sorted_figs=sorted(figs.items(), key=operator.itemgetter(1),reverse=True)
          try:
              count_FIN=count_FIN+nums['AF_FIN']
@@ -54,10 +56,11 @@ for data in sys.stdin:
              count_AFR=count_AFR+nums['AF_AFR']
              count_OTH=count_OTH+nums['AF_OTH']
              count_raw=count_raw+nums['AF_raw']
-             count_Female=count_Female+nums['AF_Female']
              count_EAS=count_EAS+nums['AF_EAS']
              count_ASJ=count_ASJ+nums['AF_ASJ']
              count_NFE=count_NFE+nums['AF_NFE']
+             count_Female=count_Female+nums['AF_Female']
+             count_Male=count_Male+nums['AF_Male']
           except:
              continue
          predict=max(figs,key=figs.get)
@@ -66,11 +69,18 @@ for data in sys.stdin:
          comvar=sorted_figs[0][1]-sorted_figs[1][1]
          print ('Variant_I.D:',chromo,locat,alle)
          print (sorted_figs)
+         for r in sorted_figs:
+             divraw=r[1]/nums['AF_raw']
+             try:
+                 print('raw_divide:',divraw)
+             expect:
+                 continue
          if sorted_figs[0][1]>=0.5 and comvar>=0.1:
              print ('Suspected_high_ethnicity_variant:',sorted_figs[0])
          else:
              continue
          print ('Predicted_ethnicity:',predict)
-    print ('FIN_total:',count_FIN,'AMR_total:',count_AMR,'AFR_total:',count_AFR,'OTH_total:', 
-           count_OTH,'raw_total:',count_raw,'Female_total:',count_Female,'EAS_total:',count_EAS,'ASJ_total:',
+    print ('FIN_total:',count_FIN,'AMR_total:',count_AMR,'AFR_total:',count_AFR,'OTH_total:',
+           count_OTH,'raw_total:',count_raw,'Female_total:',count_Female,'Male_total:',count_Male,
+           'EAS_total:',count_EAS,'ASJ_total:',
            count_ASJ,'NFE_total:',count_NFE)
