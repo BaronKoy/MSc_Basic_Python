411774fa7ab4ecee337639d1d7f86e360e2eba71
Baron Koylass <baron.koylass.16@ucl.ac.uk>
2017-08-01 16:44:50 +0100

    Add files via upload

---
+++ HERC2.script.py
@@ -0,0 +1,16 @@
+#extracting ClinVar variants from json
+#ClinVarVariant:
+
+import json
+import objectpath
+
+with open('HERC2.json') as json_data:
+    d = json.load(json_data)
+    tree = objectpath.Tree(d)
+    for x in (tuple(tree.execute('$..sub'))):
+        if str(x).startswith('ClinVarVariant'): print(x)
+    raise Exception('jk')
+
+for ClinVar in json_data.find():
+    if 'ClinVarVariant' in ClinVar:
+        print (ClinVar['ClinVarVariant'])
